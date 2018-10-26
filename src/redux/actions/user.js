336fe94204610c6b2d7126b33bcde134d3167259
denis onsare <denisonsare@gmail.com>
2018-10-26 08:43:14 +0300

    update test token

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -2,7 +2,7 @@ import api from "../../api";
 import { USER_LOGGED_IN, USER_LOGGED_OUT } from "../types";
 import setAuthorizationHeader from "../../utils/setAuthorizationHeader";
 
-const token = "0P1gctuU35xIHygfz4uJafGbjipc9t";
+const token = "5eovJ7gmpzuEirb8n9Im50LIGVMx4Q";
 
 export const userLoggedIn = user => ({
   type: USER_LOGGED_IN,
